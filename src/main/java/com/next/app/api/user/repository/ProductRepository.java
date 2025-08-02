72b4ab6f85d10c592317ee6dddf384e75a9db013
hyeminleeee <167519616+hyeminleeee@users.noreply.github.com>
2025-08-02 15:05:32 +0900

    Revert "tickets Table 추가"

--- src/main/java/com/next/app/api/user/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.next.app.api.user.repository;
-
-import com.next.app.api.user.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
--- src/main/java/com/next/app/api/user/repository/TicketRepository.java
+++
@@ -1,9 +0,0 @@
-package com.next.app.api.user.repository;
-
-import com.next.app.api.user.entity.Ticket;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TicketRepository extends JpaRepository<Ticket,Long> {
-}
